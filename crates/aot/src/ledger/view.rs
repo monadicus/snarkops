16e76eed75ec915779e791359e7ec53a3ecfedd6
gluax <16431709+gluax@users.noreply.github.com>
2024-03-18 17:39:44 -0700

    feat(aot): view accont records

--- crates/aot/src/ledger/view.rs
+++ crates/aot/src/ledger/view.rs
@@ -2,14 +2,20 @@ use std::str::FromStr;
 
 use anyhow::Result;
 use clap::Subcommand;
+use snarkvm::{
+    console::program::{Entry, Identifier, Literal, Plaintext},
+    ledger::RecordsFilter,
+    prelude::Zero,
+};
 
-use crate::{ledger::util, Address, DbLedger};
+use crate::{ledger::util, Address, DbLedger, PrivateKey, ViewKey};
 
 #[derive(Debug, Subcommand)]
 pub enum View {
     Top,
     Block { block_height: u32 },
     Balance { address: String },
+    Records { private_key: PrivateKey },
 }
 
 impl View {
@@ -26,6 +32,28 @@ impl View {
                 let addr = Address::from_str(&address)?;
 
                 println!("{address} balance {}", util::get_balance(addr, ledger)?);
+            }
+            View::Records { private_key } => {
+                let view_key = ViewKey::try_from(private_key)?;
+
+                let microcredits = Identifier::from_str("microcredits").unwrap();
+                let records = ledger
+                    .find_records(&view_key, RecordsFilter::SlowUnspent(private_key))
+                    .unwrap()
+                    .filter(|(_, record)| match record.data().get(&microcredits) {
+                        Some(Entry::Private(Plaintext::Literal(Literal::U64(amount), _))) => {
+                            !amount.is_zero()
+                        }
+                        _ => false,
+                    })
+                    .collect::<indexmap::IndexMap<_, _>>();
+
+                let address = Address::try_from(private_key)?;
+
+                println!(
+                    "{address} records {}",
+                    serde_json::to_string_pretty(&records)?
+                );
             }
         }
         Ok(())
