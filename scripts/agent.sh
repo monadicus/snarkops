5ce96e226b6e23c8938a6dab51adc9f5316e4107
Meshiest <gpg@reheatedcake.io>
2024-03-21 23:00:10 -0500

    agent and control plane scripts

---
+++ scripts/agent.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# spin up an agent with unique ports
+
+INDEX="$1"
+
+# check if index is set
+if [ -z "$INDEX" ]; then
+  echo "Usage: $0 <node_id>"
+  exit 1
+fi
+
+DATA_PATH="$(pwd)/snot-data/$INDEX"
+
+echo "Starting ${DATA_PATH}"
+cargo run --release -p snot-agent -- \
+  --path "$DATA_PATH" \
+  --bind_addr "0.0.0.0" \
+  --bft "500$INDEX" \
+  --rest "303$INDEX" \
+  --node "413$INDEX"
