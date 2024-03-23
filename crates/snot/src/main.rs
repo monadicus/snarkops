23579ad9cd39752c2349b36d1990bfbb2766ccf8
gluax <16431709+gluax@users.noreply.github.com>
2024-03-22 19:21:20 -0700

    feat(snot): more improved snot logging

--- crates/snot/src/main.rs
+++ crates/snot/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 use clap::Parser;
 use cli::Cli;
 use tracing::level_filters::LevelFilter;
@@ -19,10 +21,14 @@ async fn main() {
 
     let env_filter = env_filter
         .parse_lossy("")
+        .add_directive("tungstenite=off".parse().unwrap())
+        .add_directive("tokio_tungstenite=off".parse().unwrap())
         .add_directive("tarpc::client=ERROR".parse().unwrap())
         .add_directive("tarpc::server=ERROR".parse().unwrap());
 
-    let output = tracing_subscriber::fmt::layer();
+    let (stdout, _guard) = tracing_appender::non_blocking(io::stdout());
+
+    let output = tracing_subscriber::fmt::layer().with_writer(stdout);
 
     let output = if cfg!(debug_assertions) {
         output.with_file(true).with_line_number(true)
