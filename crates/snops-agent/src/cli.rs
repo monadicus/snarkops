cae1db288dcd569b1717183be0464218370c50f9
Meshiest <gpg@reheatedcake.io>
2024-04-20 00:40:30 -0500

    feat(agent): ignore empty labels

--- crates/snops-agent/src/cli.rs
+++ crates/snops-agent/src/cli.rs
@@ -85,7 +85,15 @@ impl Cli {
         // add &labels= if id is present
         if let Some(labels) = &self.labels {
             info!("using labels: {:?}", labels);
-            query.push_str(&format!("&labels={}", labels.join(",")));
+            query.push_str(&format!(
+                "&labels={}",
+                labels
+                    .iter()
+                    .filter(|s| s.is_empty())
+                    .cloned()
+                    .collect::<Vec<_>>()
+                    .join(",")
+            ));
         }
 
         let (is_tls, host) = endpoint
