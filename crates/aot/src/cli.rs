158b07543df31f26fed062befea3bd3d4073c648
Meshiest <gpg@reheatedcake.io>
2024-04-27 11:08:31 -0500

    Revert "refactor: untested alternative to tokio runtime"
    This reverts commit 5e38a0b3cc049ec552719ecdaacbce13e34fc1dd.

--- crates/aot/src/cli.rs
+++ crates/aot/src/cli.rs
@@ -217,7 +217,11 @@ impl Cli {
             }
 
             let (layer, task) = builder.build_url(loki.to_owned()).expect("bad loki url");
-            thread::spawn(|| futures::executor::block_on(task));
+            thread::spawn(|| {
+                let rt = tokio::runtime::Runtime::new().unwrap();
+                let handle = rt.spawn(task);
+                rt.block_on(handle).unwrap();
+            });
             layers.push(Box::new(layer.with_filter(filter3)));
         }
 
