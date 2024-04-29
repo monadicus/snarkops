a2346d3d7baf5b2177c46c4b9245a9e874bfd255
gluax <16431709+gluax@users.noreply.github.com>
2024-04-28 21:44:45 -0600

    refactor(cp): undo untagged on EnvNodeState

--- crates/snops/src/env/mod.rs
+++ crates/snops/src/env/mod.rs
@@ -71,7 +71,6 @@ pub struct TxPipes {
 
 /// The effective test state of a node.
 #[derive(Debug, Clone, Serialize)]
-#[serde(untagged)]
 #[allow(clippy::large_enum_variant)]
 pub enum EnvNodeState {
     Internal(Node),
