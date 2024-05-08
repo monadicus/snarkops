3b8b393af4e250710cb321c1036d39cf06bf9368
Meshiest <gpg@reheatedcake.io>
2024-05-07 20:01:28 -0500

    fix(persist): fix env persist missing fields, add tests

--- crates/snops/src/persist/env.rs
+++ crates/snops/src/persist/env.rs
@@ -26,7 +26,7 @@ use crate::{
 };
 
 #[derive(Clone)]
-pub struct PersistEnvHeader {
+pub struct PersistEnvFormatHeader {
     version: u8,
     nodes: PersistNodeFormatHeader,
     tx_source: TxSourceFormatHeader,
@@ -165,7 +165,7 @@ impl PersistEnv {
     }
 }
 
-impl DataFormat for PersistEnvHeader {
+impl DataFormat for PersistEnvFormatHeader {
     type Header = u8;
     const LATEST_HEADER: Self::Header = 1;
 
@@ -198,7 +198,7 @@ impl DataFormat for PersistEnvHeader {
         let tx_source = read_dataformat(reader)?;
         let tx_sink = read_dataformat(reader)?;
 
-        Ok(PersistEnvHeader {
+        Ok(PersistEnvFormatHeader {
             version,
             nodes,
             tx_source,
@@ -208,8 +208,8 @@ impl DataFormat for PersistEnvHeader {
 }
 
 impl DataFormat for PersistEnv {
-    type Header = PersistEnvHeader;
-    const LATEST_HEADER: Self::Header = PersistEnvHeader {
+    type Header = PersistEnvFormatHeader;
+    const LATEST_HEADER: Self::Header = PersistEnvFormatHeader {
         version: 1,
         nodes: PersistNode::LATEST_HEADER, // TODO: use PersistNode::LATEST_HEADER
         tx_source: TxSource::LATEST_HEADER,
@@ -222,8 +222,11 @@ impl DataFormat for PersistEnv {
     ) -> Result<usize, snops_common::format::DataWriteError> {
         let mut written = 0;
 
+        written += writer.write_data(&self.id)?;
         written += writer.write_data(&self.storage_id)?;
         written += writer.write_data(&self.nodes)?;
+        written += writer.write_data(&self.tx_pipe_drains)?;
+        written += writer.write_data(&self.tx_pipe_sinks)?;
         written += writer.write_data(&self.cannon_configs)?;
 
         Ok(written)
@@ -258,4 +261,84 @@ impl DataFormat for PersistEnv {
             cannon_configs,
         })
     }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use std::str::FromStr;
+
+    use snops_common::{
+        format::{read_dataformat, write_dataformat, DataFormat},
+        state::InternedId,
+    };
+
+    use crate::{
+        cannon::{sink::TxSink, source::TxSource},
+        persist::{
+            PersistEnv, PersistEnvFormatHeader, PersistNode, PersistNodeFormatHeader,
+            TxSinkFormatHeader, TxSourceFormatHeader,
+        },
+    };
+
+    macro_rules! case {
+        ($name:ident, $ty:ty, $a:expr, $b:expr) => {
+            #[test]
+            fn $name() -> Result<(), Box<dyn std::error::Error>> {
+                let mut data = Vec::new();
+                write_dataformat(&mut data, &$a)?;
+                assert_eq!(data, $b);
+
+                let mut reader = &data[..];
+                let read_value = read_dataformat::<_, $ty>(&mut reader)?;
+
+                // write the data again because not every type implements PartialEq
+                let mut data2 = Vec::new();
+                write_dataformat(&mut data2, &read_value)?;
+                assert_eq!(data, data2);
+                Ok(())
+            }
+        };
+    }
+
+    case!(
+        env_header,
+        PersistEnvFormatHeader,
+        PersistEnv::LATEST_HEADER,
+        [
+            PersistEnvFormatHeader::LATEST_HEADER.to_byte_vec()?,
+            PersistEnv::LATEST_HEADER.version.to_byte_vec()?,
+            PersistNodeFormatHeader::LATEST_HEADER.to_byte_vec()?,
+            PersistNode::LATEST_HEADER.to_byte_vec()?,
+            TxSourceFormatHeader::LATEST_HEADER.to_byte_vec()?,
+            TxSource::LATEST_HEADER.to_byte_vec()?,
+            TxSinkFormatHeader::LATEST_HEADER.to_byte_vec()?,
+            TxSink::LATEST_HEADER.to_byte_vec()?,
+        ]
+        .concat()
+    );
+
+    case!(
+        env,
+        PersistEnv,
+        PersistEnv {
+            id: InternedId::from_str("foo")?,
+            storage_id: InternedId::from_str("bar")?,
+            nodes: Default::default(),
+            tx_pipe_drains: Default::default(),
+            tx_pipe_sinks: Default::default(),
+            cannon_configs: Default::default()
+        },
+        [
+            PersistEnvFormatHeader::LATEST_HEADER.to_byte_vec()?,
+            PersistEnv::LATEST_HEADER.to_byte_vec()?,
+            InternedId::from_str("foo")?.to_byte_vec()?,
+            InternedId::from_str("bar")?.to_byte_vec()?,
+            Vec::<(String, PersistNode)>::new().to_byte_vec()?,
+            Vec::<InternedId>::new().to_byte_vec()?,
+            Vec::<InternedId>::new().to_byte_vec()?,
+            Vec::<(InternedId, TxSource, TxSink)>::new().to_byte_vec()?,
+        ]
+        .concat()
+    );
 }
