1a41590c14ba4b55afb1c482aacc4bc7281ecf63
gluax <16431709+gluax@users.noreply.github.com>
2024-05-12 13:09:55 -0600

    refactor: add find agents endpoint

--- crates/snops-cli/src/commands/agent.rs
+++ crates/snops-cli/src/commands/agent.rs
@@ -1,8 +1,9 @@
 use std::str::FromStr;
 
 use anyhow::Result;
-use clap::{error::ErrorKind, CommandFactory, Parser, ValueHint};
+use clap::{error::ErrorKind, ArgGroup, CommandFactory, Parser, ValueHint};
 use reqwest::blocking::{Client, Response};
+use serde_json::json;
 use snops_common::state::AgentId;
 
 use super::DUMMY_ID;
@@ -21,6 +22,40 @@ pub struct Agent {
 /// Env commands
 #[derive(Debug, Parser)]
 enum AgentCommands {
+    /// Find agents by set criteria.
+    /// If all of client/compute/prover/validator are not specified it can be
+    /// any one of them.
+    #[clap(group(ArgGroup::new("environment").required(false).args(&["env", "all"])))]
+    Find {
+        /// Whether the agent can be a client.
+        #[clap(long)]
+        client: bool,
+        /// Whether the agent can be a compute.
+        #[clap(long)]
+        compute: bool,
+        /// Whether the agent can be a prover.
+        #[clap(long)]
+        prover: bool,
+        /// Whether the agent can be a validator.
+        #[clap(long)]
+        validator: bool,
+        /// Which env you are finding the agens from.
+        /// Not specifing a env, means only inventoried agents are found.
+        #[clap(long, group = "environment")]
+        env: Option<String>,
+        /// Means regardless of connection status, and state we find them.
+        #[clap(long, group = "environment")]
+        all: bool,
+        /// The labels an agent should have.
+        #[clap(long, value_delimiter = ',', num_args = 1..)]
+        labels: Vec<String>,
+        /// If the agent has a local private key or not.
+        #[clap(long)]
+        local_pk: bool,
+        /// Wether to include offline agents as well.
+        #[clap(long)]
+        include_offline: bool,
+    },
     /// Get the specific agent.
     #[clap(alias = "i")]
     Info,
@@ -38,6 +73,36 @@ impl Agent {
     pub fn run(self, url: &str, client: Client) -> Result<Response> {
         use AgentCommands::*;
         Ok(match self.command {
+            Find {
+                env,
+                labels,
+                all,
+                include_offline,
+                local_pk,
+                client: mode_client,
+                compute,
+                prover,
+                validator,
+            } => {
+                let ep = format!("{url}/api/v1/agents/find");
+
+                client
+                    .post(ep)
+                    .json(&json!({
+                        "mode": {
+                          "client": mode_client,
+                          "compute": compute,
+                          "prover": prover,
+                          "validator": validator,
+                        },
+                        "env": env,
+                        "labels": labels,
+                        "all": all,
+                        "include_offline": include_offline,
+                        "local_pk": local_pk,
+                    }))
+                    .send()?
+            }
             List => {
                 let ep = format!("{url}/api/v1/agents");
 
