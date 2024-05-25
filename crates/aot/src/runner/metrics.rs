0294973917995614f39c0bc93c23b2a9e869f8af
Meshiest <gpg@reheatedcake.io>
2024-05-24 21:44:11 -0400

    chore(aot): add new metrics

--- crates/aot/src/runner/metrics.rs
+++ crates/aot/src/runner/metrics.rs
@@ -3,9 +3,12 @@
 
 use snarkos_node_metrics::{bft, blocks, consensus, router, tcp};
 
-pub const COUNTER_NAMES: [&str; 1] = [bft::LEADERS_ELECTED];
+pub const COUNTER_NAMES: [&str; 2] = [
+    bft::LEADERS_ELECTED,
+    consensus::STALE_UNCONFIRMED_TRANSMISSIONS,
+];
 
-pub const GAUGE_NAMES: [&str; 19] = [
+pub const GAUGE_NAMES: [&str; 26] = [
     bft::CONNECTED,
     bft::CONNECTING,
     bft::LAST_STORED_ROUND,
@@ -13,8 +16,15 @@ pub const GAUGE_NAMES: [&str; 19] = [
     bft::CERTIFIED_BATCHES,
     bft::HEIGHT,
     bft::LAST_COMMITTED_ROUND,
+    bft::IS_SYNCED,
     blocks::SOLUTIONS,
     blocks::TRANSACTIONS,
+    blocks::ACCEPTED_DEPLOY,
+    blocks::ACCEPTED_EXECUTE,
+    blocks::REJECTED_DEPLOY,
+    blocks::REJECTED_EXECUTE,
+    blocks::ABORTED_TRANSACTIONS,
+    blocks::ABORTED_SOLUTIONS,
     blocks::PROOF_TARGET,
     blocks::COINBASE_TARGET,
     blocks::CUMULATIVE_PROOF_TARGET,
