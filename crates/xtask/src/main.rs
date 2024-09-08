3d35d9b4c8f4a1eab6c33d295a873f49ed608e8f
gluax <16431709+gluax@users.noreply.github.com>
2024-09-08 14:35:36 -0700

    refactor: wip removing cargo make

--- crates/xtask/src/main.rs
+++ crates/xtask/src/main.rs
@@ -1,9 +1,138 @@
 #![allow(dead_code)]
 
-use std::{env, process::Command};
-
-use anyhow::{bail, Context, Result};
+use anyhow::{Context, Result};
+use clap::{Parser, Subcommand, ValueEnum};
 use xshell::{cmd, Shell};
+
+#[derive(Parser)]
+struct Args {
+    #[clap(subcommand)]
+    subcommand: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    /// Generates markdown files for all the CLIs in the codebase.
+    Clipages,
+    /// Generates manpages for all the CLIs in the codebase.
+    Manpages,
+    /// Format the codebase
+    Fmt {
+        /// Check if the codebase is formatted rather than formatting it
+        #[clap(short, long)]
+        check: bool,
+    },
+    /// Lint the codebase with clippy
+    Lint,
+    /// Checks for unused dependencies in the codebase
+    UDeps {
+        /// Fix unused dependencies
+        #[clap(short, long)]
+        fix: bool,
+    },
+
+    #[cfg(target_os = "linux")]
+    /// Install's UPX only on linux.
+    InstallUpx,
+    /// Builds the project
+    Build(Build),
+}
+
+#[derive(Parser)]
+struct Build {
+    #[clap(long)]
+    compress: bool,
+    #[clap(short, long, default_value = "release-big")]
+    profile: Profile,
+    #[clap(long)]
+    cranelift: bool,
+    target: BuildTarget,
+}
+
+impl Build {
+    fn run(self, sh: &Shell) -> Result<()> {
+        let profile = self.profile.as_ref();
+        let package = self.target.as_ref();
+        // -Zbuild-std=std,panic_abort -Zbuild-std-features=panic_immediate_abort
+        let cmd = cmd!(sh, "cargo build");
+        let cmd = if matches!(self.target, BuildTarget::All) {
+            cmd
+        } else {
+            cmd.arg("-p").arg(package)
+        };
+
+        let cmd = cmd.arg("--profile").arg(profile);
+        // let cmd = cmd.arg("--target x86_64-unknown-linux-gnu");
+
+        let cmd = if self.cranelift {
+            cmd.arg("-Zcodegen-backend=cranelift")
+        } else {
+            cmd
+        };
+
+        cmd.run()?;
+
+        Ok(())
+    }
+}
+
+#[derive(Clone, ValueEnum)]
+enum BuildTarget {
+    All,
+    Aot,
+    Snops,
+    SnopsAgent,
+    SnopsCli,
+}
+
+impl AsRef<str> for BuildTarget {
+    fn as_ref(&self) -> &str {
+        match self {
+            BuildTarget::All => "",
+            BuildTarget::Aot => "-snarkos-aot",
+            BuildTarget::Snops => "snops",
+            BuildTarget::SnopsAgent => "snops-agent",
+            BuildTarget::SnopsCli => "snops-cli",
+        }
+    }
+}
+
+#[derive(Clone, ValueEnum)]
+enum Profile {
+    ReleaseBig,
+    ReleaseSmall,
+    Debug,
+}
+
+impl AsRef<str> for Profile {
+    fn as_ref(&self) -> &str {
+        match self {
+            Profile::ReleaseBig => "release-big",
+            Profile::ReleaseSmall => "release-small",
+            Profile::Debug => "debug",
+        }
+    }
+}
+
+impl Command {
+    fn run(self, sh: &Shell) -> Result<()> {
+        match self {
+            Command::Clipages => clipages(sh),
+            Command::Manpages => manpages(sh),
+            Command::Fmt { check } => fmt(sh, check),
+            Command::Lint => cmd!(
+                sh,
+                "cargo +nightly clippy --all-targets --all-features -- -D warnings"
+            )
+            .run()
+            .context("Running clippy"),
+            Command::UDeps { fix } => udeps(sh, fix),
+            #[cfg(target_os = "linux")]
+            Command::InstallUpx => install_upx(sh),
+            Command::Build(build) => build.run(sh),
+        }
+    }
+}
 
 fn main() {
     if let Err(e) = try_main() {
@@ -12,30 +141,20 @@ fn main() {
     }
 }
 
-const TASKS: &[&str] = &["help", "clipages", "manpages"];
-
 fn try_main() -> Result<()> {
+    let sh = Shell::new()?;
+
     // Ensure our working directory is the toplevel
     {
-        let toplevel_path = Command::new("git")
-            .args(["rev-parse", "--show-toplevel"])
-            .output()
-            .context("Invoking git rev-parse")?;
-        if !toplevel_path.status.success() {
-            bail!("Failed to invoke git rev-parse");
-        }
-        let path = String::from_utf8(toplevel_path.stdout)?;
+        let path = cmd!(&sh, "git rev-parse --show-toplevel")
+            .read()
+            .context("Failed to invoke git rev-parse")?;
         std::env::set_current_dir(path.trim()).context("Changing to toplevel")?;
     }
 
-    let task = env::args().nth(1);
-    let sh = Shell::new()?;
-    match task.as_deref() {
-        Some("help") => print_help()?,
-        Some("clipages") => clipages(&sh)?,
-        Some("manpages") => manpages(&sh)?,
-        _ => print_help()?,
-    }
+    let args = Args::parse();
+
+    args.subcommand.run(&sh)?;
 
     Ok(())
 }
@@ -60,10 +179,45 @@ fn manpages(sh: &Shell) -> Result<()> {
     Ok(())
 }
 
-fn print_help() -> Result<()> {
-    println!("Tasks:");
-    for name in TASKS {
-        println!("  - {name}");
+fn fmt(sh: &Shell, check: bool) -> Result<()> {
+    let cmd = cmd!(sh, "cargo +nightly fmt --all");
+    let cmd = if check { cmd.arg("-- --check") } else { cmd };
+
+    cmd.run()?;
+    Ok(())
+}
+
+fn insall_cargo_subcommands(sh: &Shell, subcmd: &'static str) -> Result<()> {
+    cmd!(sh, "cargo install {subcmd} --locked").run()?;
+    Ok(())
+}
+
+fn udeps(sh: &Shell, fix: bool) -> Result<()> {
+    insall_cargo_subcommands(sh, "cargo-machete")?;
+    let cmd = cmd!(sh, "cargo-machete");
+    let cmd = if fix { cmd.arg("fix") } else { cmd };
+    cmd.run()?;
+    Ok(())
+}
+
+#[cfg(target_os = "linux")]
+fn install_upx(sh: &Shell) -> Result<()> {
+    // Check if upx is already installed and return early if it is
+    if !cmd!(sh, "command -v upx").read()?.is_empty() {
+        return Ok(());
     }
+
+    cmd!(
+        sh,
+        "wget https://github.com/upx/upx/releases/download/v4.2.3/upx-4.2.3-amd64_linux.tar.xz"
+    )
+    .run()?;
+    cmd!(sh, "tar -xf upx-4.2.3-amd64_linux.tar.xz").run()?;
+    cmd!(sh, "cp ./upx-4.2.3-amd64_linux/upx /usr/local/bin/").run()?;
+    cmd!(
+        sh,
+        "rm -rf upx-4.2.3-amd64_linux.tar.xz upx-4.2.3-amd64_linux/"
+    )
+    .run()?;
     Ok(())
 }
