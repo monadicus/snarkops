7a26c110dbfad73bd242cd2fac27748554670f08
Meshiest <gpg@reheatedcake.io>
2024-11-28 03:56:51 -0500

    fix(controlplane): online/offline/reboot actions properly wait for reconcile complete

--- crates/controlplane/src/server/actions/power.rs
+++ crates/controlplane/src/server/actions/power.rs
@@ -4,15 +4,57 @@ use axum::{
     response::{IntoResponse, Response},
     Json,
 };
-use snops_common::action_models::WithTargets;
+use snops_common::{
+    action_models::WithTargets,
+    node_targets::NodeTargets,
+    state::{AgentId, AgentState, EnvId},
+};
+use tracing::info;
 
 use super::Env;
-use crate::state::pending_reconcile_node_map;
+use crate::state::{pending_reconcile_node_map, GlobalState};
+
+async fn wait_for_nodes(
+    state: &GlobalState,
+    env_id: EnvId,
+    nodes: NodeTargets,
+    pending: Vec<(AgentId, AgentState)>,
+) -> Response {
+    let mut awaiting_agents = pending.iter().map(|a| a.0).collect::<HashSet<_>>();
+    let node_map = pending_reconcile_node_map(pending.iter());
+
+    // create the subscriber before updating agent states in order to
+    // avoid missing any events
+    use crate::events::prelude::*;
+    let mut subscriber = state
+        .events
+        .subscribe_on(NodeTargetIs(nodes) & EnvIs(env_id) & ReconcileComplete);
+
+    state.update_agent_states(pending).await;
+
+    // wait at most 30 seconds for all agents to reconcile
+    let expires = tokio::time::Instant::now() + std::time::Duration::from_secs(30);
+    while !awaiting_agents.is_empty() {
+        tokio::select! {
+            _ = tokio::time::sleep_until(expires) => {
+                break;
+            }
+            Ok(event) = subscriber.next() => {
+                if let Some(agent) = event.agent {
+                    awaiting_agents.remove(&agent);
+                }
+            }
+        }
+    }
+
+    Json(node_map).into_response()
+}
 
 pub async fn online(
     Env { env, state, .. }: Env,
     Json(WithTargets { nodes, .. }): Json<WithTargets>,
 ) -> Response {
+    info!("env {} invoked online action for {nodes}", env.id);
     let pending = env
         .matching_agents(&nodes, &state.pool)
         .filter_map(|a| {
@@ -23,35 +65,16 @@ pub async fn online(
                 })
             })
         })
-        .collect::<Vec<_>>(); // TODO
+        .collect::<Vec<_>>();
 
-    let mut awaiting_agents = pending.iter().map(|a| a.0).collect::<HashSet<_>>();
-
-    let node_map = pending_reconcile_node_map(pending.iter());
-
-    state.update_agent_states(pending).await;
-
-    use crate::events::prelude::*;
-    let mut subscriber = state
-        .events
-        .subscribe_on(NodeTargetIs(nodes) & EnvIs(env.id) & ReconcileComplete);
-
-    while !awaiting_agents.is_empty() {
-        // TODO: expire after some time
-        if let Ok(event) = subscriber.next().await {
-            if let Some(agent) = event.agent {
-                awaiting_agents.remove(&agent);
-            }
-        }
-    }
-
-    Json(node_map).into_response()
+    wait_for_nodes(&state, env.id, nodes, pending).await
 }
 
 pub async fn offline(
     Env { env, state, .. }: Env,
     Json(WithTargets { nodes, .. }): Json<WithTargets>,
 ) -> Response {
+    info!("env {} invoked offline action for {nodes}", env.id);
     let pending = env
         .matching_agents(&nodes, &state.pool)
         .filter_map(|a| {
@@ -62,19 +85,13 @@ pub async fn offline(
                 })
             })
         })
-        .collect::<Vec<_>>(); // TODO
+        .collect::<Vec<_>>();
 
-    let node_map = pending_reconcile_node_map(pending.iter());
-
-    state.update_agent_states(pending).await;
-
-    Json(node_map).into_response()
+    wait_for_nodes(&state, env.id, nodes, pending).await
 }
 
 pub async fn reboot(env: Env, json: Json<WithTargets>) -> Response {
     let offline_res = offline(env.clone(), json.clone()).await;
-
-    // TODO: wait for nodes to reconcile offline
 
     if !offline_res.status().is_success() {
         offline_res
